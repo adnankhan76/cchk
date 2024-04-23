0bbeb4fcfa7091ebde37868b2b129ccf1d209ddf
adnankhan76 <150260782+adnankhan76@users.noreply.github.com>
2024-04-23 21:47:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ app.get('/view/card/:number', (req, res) => {
 });
 
 // Start the Express server
-app.listen(3000, () => {
+app.listen(8080, () => {
     console.log('Express server is running on port 3000');
 });
